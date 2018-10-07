e61ad52f8a358798e038fe169118c867d690a72f
Raphael Moutard <moutardr@amazon.com>
2018-10-07 21:14:43 +0200

    Clean useless file

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='picasso',
-    packages=['picasso'],
-    version='0.0.7',
-    description='Test project to get acquainted with TravisCI',
-    url='https://github.com/moutard/picasso',
-)
